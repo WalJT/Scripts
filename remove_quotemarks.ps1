480612043ea95bf67a025effc86d6b1428e62b01
WalJT <jttw259@zoho.com>
2022-10-22 09:23:52 +0100

    Create plan for quote marks powershell script

---
+++ remove_quotemarks.ps1
@@ -0,0 +1,8 @@
+
+<#
+
+1. [ ] Write a function that removes surrounding quotemarks from a file name
+2. [ ] Use this function to loop through multiple file names
+3. [ ] Get list as input and filter to only files that have quotemarks at the end of the extension
+
+#>
