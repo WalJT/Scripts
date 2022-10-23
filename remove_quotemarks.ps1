d49bc791c4104a9793e54a5ea0a9518deb697771
WalJT <jttw259@zoho.com>
2022-10-23 09:22:23 +0100

    Step 1 complete

--- remove_quotemarks.ps1
+++ remove_quotemarks.ps1
@@ -1,8 +1,29 @@
 
 <#
+A specific use case script to remove quote marks surrounding file names
 
-1. [ ] Write a function that removes surrounding quotemarks from a file name
-2. [ ] Use this function to loop through multiple file names
-3. [ ] Get list as input and filter to only files that have quotemarks at the end of the extension
+1. [x] Write a function that removes surrounding quotemarks from a string
+2. [ ] Appy this to renaming files
+3. [ ] Use this function to loop through multiple file names
+4. [ ] Get list as input and filter to only files that have quotemarks at the end of the extension
 
 #>
+
+function Remove-Quotes {
+    param (
+        $InputFile
+    )
+    
+    $OutputFile = $InputFile -replace "'", ""
+
+    Write-Host $InputFile
+    Write-Host $OutputFile
+}
+
+$test1 = "'file.xlsx'"
+$test2 = "'Annual Report.xls'"
+$test3 = "'Hitchikers Guiede to the Galaxy.pdf'"
+
+Remove-Quotes $test1
+Remove-Quotes $test2
+Remove-Quotes $test3
