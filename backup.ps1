437d8c189a7e3d3bde03ba83223ce93db18466e0
WalJT <noreply@juliantwalsh.ie>
2024-07-18 14:00:35 +0100

    Add base for backup powershell script

---
+++ backup.ps1
@@ -0,0 +1,9 @@
+
+# Todo:
+# 1. Get input for source folder
+# 2. Get input for destination path (not archive name)
+# 3. Get input for root of file name + append date
+# 4. GUI with file pickers?
+
+
+Compress-Archive -Path .\Test\ -DestinationPath .\test.zip 
