5a092b77250bc1321219b7804d17e6512c8dcd63
WalJT <noreply@juliantwalsh.ie>
2026-07-05 10:17:52 +0100

    Update nvim config

--- init.lua
+++ init.lua
@@ -10,17 +10,22 @@ vim.o.expandtab = true
 vim.o.tabstop = num_tabs
 vim.o.softtabstop = num_tabs
 vim.o.shiftwidth = num_tabs
+vim.g.mapleader = " "
 
 
 -- Plugins
 vim.pack.add({
   'https://github.com/rebelot/kanagawa.nvim',
-  'https://github.com/nvim-tree/nvim-web-devicons',
-  'https://github.com/nvim-lualine/lualine.nvim'
+  'https://github.com/nvim-lualine/lualine.nvim',
+  { src = 'https://github.com/nvim-mini/mini.nvim', version = 'stable' },
+
 })
 
 -- Set theme
 vim.cmd([[colorscheme kanagawa-dragon]])
+
+-- mini.pick file picker
+require('mini.pick').setup({})
 
 -- Configure Lualine
 require('lualine').setup()
